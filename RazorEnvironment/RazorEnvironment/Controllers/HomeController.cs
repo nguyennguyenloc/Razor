a6f3fc3bfa893bf51721b4f4215f6fb8129b8d75
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-10-24 09:37:03 +0700

    Done

---
+++ RazorEnvironment/RazorEnvironment/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorEnvironment.Model;
+
+namespace RazorEnvironment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           Student objStudent = new Student();
+            //objStudent.Name = "Nguyên Lộc";
+            objStudent.Age = 22;
+            return View(objStudent);
+        }
+    }
+}
---
+++ RazorEnvironment/RazorEnvironment/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorEnvironment.Model
+{
+    public class Student
+    {
+        public Student()
+        {
+            this.Name = "default";
+        }
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
